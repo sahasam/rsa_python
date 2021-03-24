d5a1960d854c59087742c4883c898a415156888d
Sahas Munamala <munamalasahas@gmail.com>
2021-03-24 14:50:18 -0700

    initial project directory setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rsapy',
+    version='1.0',
+    description='Python RSA encryption implementation in python',
+    author='Sahas Munamala',
+    author_email='gward@python.net',
+    url='https://github.com/sahasam/rsa_python',
+    packages=['rsapy'],
+)
